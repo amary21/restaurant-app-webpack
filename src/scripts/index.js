aed08f596e0ea70fb9062bb956843b5cec0ee770
amary21 <taufik.amary@gmail.com>
2020-09-15 11:21:07 +0700

    app-bar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,8 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 
 console.log('Hello Coders! :)');
