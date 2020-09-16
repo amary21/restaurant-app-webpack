716bec735580097d0d678a6abf4854fedb674160
amary21 <taufik.amary@gmail.com>
2020-09-16 13:17:33 +0700

    content

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,8 @@ import loadData from './load-data.js'
 
 const main = ()=>{
     const menuToggle = document.querySelector('.menu-toggle input');
-    const nav = document.querySelector('nav ul');
+    const nav = document.querySelector('nav');
+    const navSideBar = document.querySelector('nav ul');
 
     const disableScroll = () => { 
         document.body.classList.add("stop-scrolling"); 
@@ -10,18 +11,36 @@ const main = ()=>{
       
     const enableScroll = () => { 
         document.body.classList.remove("stop-scrolling"); 
-    } 
+    }
+    
+    const changeColorNav = () =>{
+        window.onscroll = () => {
+            if(screen.width > 576){
+                nav.classList.add("nav-colored");
+                if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
+                    nav.classList.add("nav-colored");
+                    nav.classList.remove("nav-transparent");
+                } 
+                else {
+                    nav.classList.add("nav-transparent");
+                    nav.classList.remove("nav-colored");
+                }
+            }
+        };
+    }
     
 
     document.addEventListener("DOMContentLoaded", ()=>{
         menuToggle.addEventListener('click', ()=> {
-            nav.classList.toggle('slide');
-            if(nav.className == 'slide'){
+            navSideBar.classList.toggle('slide');
+            if(navSideBar.className == 'slide'){
                 disableScroll();
             } else {
                 enableScroll();
             }
         });
+
+        changeColorNav();
 
         loadData();
     })
