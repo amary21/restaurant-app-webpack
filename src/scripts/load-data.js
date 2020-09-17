a3d64e098b7f7dce0f3b355f9ef1af620bceb03a
Taufik <taufik.amary@gmail.com>
2020-09-17 19:54:19 +0700

    fix-size

--- src/scripts/load-data.js
+++ src/scripts/load-data.js
@@ -5,7 +5,7 @@ const loadData = () => {
     data.restaurants.forEach(restaurant => {
         const itemElement = document.createElement("article");
         itemElement.setAttribute("class", "post-item");
-        itemElement.innerHTML = `<img class="post-item__thumbnail" src="${restaurant.pictureId}" alt="image ${restaurant.name}">
+        itemElement.innerHTML = `<img class="post-item__thumbnail" src="${restaurant.pictureId}" alt="${restaurant.name}">
             <div class="post-item__content">
                 <h1 class="post-item__name">${restaurant.name} (${restaurant.city})</h1>
                 <h4 class="post-item__rate">${restaurant.rating}</h4>
